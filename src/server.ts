97b011f9c733aa5c4f07f8c6763aa1f8c576d7bd
Jordan100X <jts1022@yahoo.com>
2021-06-16 18:46:21 -0400

    Implemented image filtering

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,117 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
+  // Get a greeting to a specific person 
+  // to demonstrate routing parameters
+  // > try it {{host}}/persons/:the_name
+  app.get( "/persons/:name", async ( req, res ) => {
+    let { name } = req.params;
+
+    if ( !name ) {
+      return res.status(400)
+                .send(`name is required`);
+    }
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${name}!`);
+} );
+
+// Get a greeting to a specific person to demonstrate req.query
+// > try it {{host}}/persons?name=the_name
+app.get( "/persons/", async ( req, res ) => {
+  let { name } = req.query;
+
+  if ( !name ) {
+    return res.status(400)
+              .send(`name is required`);
+  }
+
+  return res.status(200)
+            .send(`Welcome to the Cloud, ${name}!`);
+} );
+
+// Post a greeting to a specific person
+// to demonstrate req.body
+// > try it by posting {"name": "the_name" } as 
+// an application/json body to {{host}}/persons
+app.post( "/persons", async ( req, res ) => {
+
+    const { name } = req.body;
+
+    if ( !name ) {
+      return res.status(400)
+                .send(`name is required`);
+    }
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${name}!`);
+} );
+//! END
+
+  // Get a greeting to a specific person 
+  // to demonstrate routing parameters
+  // > try it {{host}}/persons/:the_name
+  app.get( "/filteredimage/:image_url", async ( req, res ) => {
+    let { image_url } = req.params;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`url is required`);
+    }
+
+    let file = await filterImageFromURL(image_url);
+    res.send(file);
+    return res.status(200).sendFile(file);
+    let files: string[] = [file];
+    deleteLocalFiles(files);
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${image_url}!`);
+} );
+
+// Get a greeting to a specific person to demonstrate req.query
+// > try it {{host}}/persons?name=the_name
+app.get( "/filteredimage/", async ( req, res ) => {
+  let { image_url } = req.query;
+
+  if ( !image_url ) {
+    return res.status(400)
+              .send(`url is required`);
+  }
+
+  let file = await filterImageFromURL(image_url);
+  res.send(file);
+  return res.status(200).sendFile(file);
+  let files: string[] = [file];
+  deleteLocalFiles(files);
+
+  return res.status(200)
+            .send(`Welcome to the Cloud, ${image_url}!`);
+} );
+
+// Post a greeting to a specific person
+// to demonstrate req.body
+// > try it by posting {"name": "the_name" } as 
+// an application/json body to {{host}}/persons
+app.post( "/filteredimage", async ( req, res ) => {
+
+    const { image_url } = req.body;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`url is required`);
+    }
+
+    let file = await filterImageFromURL(image_url);
+    res.send(file);
+    return res.status(200).sendFile(file);
+    let files: string[] = [file];
+    deleteLocalFiles(files);
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${image_url}!`);
+} );
+//! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
